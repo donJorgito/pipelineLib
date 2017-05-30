72983e236a533a6f3a91948cb478157f259e0932
witokondoria <witokondoria@gmail.com>
2017-05-30 19:33:44 +0200

    adding an agent

--- vars/action.groovy
+++ vars/action.groovy
@@ -1,7 +1,9 @@
 def call(Map conf, String script) {
   if (conf.enabled) {
+    node ('agent') {
     def shStdout = sh returnStdout: true, script: "${script}"
     return shStdout
+    }
   } else {
     echo "Wont run action due configured settings"
     return "Wont run action due configured settings"
